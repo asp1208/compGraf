d8d0a8731f3a140f9d19ddd03240516afe1e117c
Галина Гуржиева <43145108+asp1208@users.noreply.github.com>
2021-10-01 15:12:10 +0300

    Add files via upload

---
+++ lab31/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿
+namespace lab31
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Location = new System.Drawing.Point(344, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(556, 431);
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(24, 37);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(107, 33);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "цвет";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(171, 37);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(63, 33);
+            this.pictureBox2.TabIndex = 11;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(24, 390);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(107, 35);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "Брезенхе́м ";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(180, 390);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(107, 35);
+            this.button3.TabIndex = 13;
+            this.button3.Text = "Ву";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(24, 90);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(107, 33);
+            this.button4.TabIndex = 14;
+            this.button4.Text = "очистить";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(24, 149);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(107, 33);
+            this.button5.TabIndex = 15;
+            this.button5.Text = "заливка 1a";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(923, 538);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ lab31/Form1.cs
@@ -0,0 +1,278 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab31
+{
+    public partial class Form1 : Form
+    {
+        Pen pen;
+        Pen fill;
+        Graphics graphics;
+        public Form1()
+        {
+            InitializeComponent();
+            pen = new Pen(Color.Black, 1);
+            fill = new Pen(Color.Black, 1);
+            pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            pictureBox2.BackColor = Color.Black;
+            graphics = Graphics.FromImage(pictureBox1.Image);
+            pictureBox1.Invalidate();
+        }
+
+
+
+        //------------------------------- #1 -----------------------------------------------
+
+        private Color area_color;
+        bool fill_flag = false;
+        private void button5_Click(object sender, EventArgs e)
+        {
+            fill_flag = true;
+        }
+
+        private bool equalColors(Color c1, Color c2)
+        {
+            return c1.R == c2.R && c1.G == c2.G && c1.B == c2.B;
+        }
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (fill_flag == false)
+            {
+                if (line)
+                {
+                    if (bresenham)
+                    {
+                        drawLineBresenham(pen, line_st_x, line_st_y, e.X, e.Y);
+                    }
+                    else if (wu)
+                    {
+                        drawLineWu(pen, line_st_x, line_st_y, e.X, e.Y);
+                    }
+                }
+                else
+                {
+                    line_st_x = e.X;
+                    line_st_y = e.Y;
+                }
+                line = !line;
+            }
+            else if (fill_flag)
+            {
+                area_color = (pictureBox1.Image as Bitmap).GetPixel(e.X, e.Y);
+                if (!equalColors(area_color, fill.Color)) 
+                        rec_fill(e.X, e.Y);
+            }
+        }
+
+        private void rec_fill(int x, int y)
+        {
+            Bitmap bitmap = pictureBox1.Image as Bitmap;
+            if (0 <= x && x < bitmap.Width && 0 <= y && y < bitmap.Height && !equalColors(area_color, fill.Color))
+            {
+                Point leftBound = new Point(x, y);
+                Point rightBound = new Point(x, y);
+                Color currentColor = area_color;
+
+                while (0 < leftBound.X && equalColors(bitmap.GetPixel(leftBound.X, y),area_color))
+                {
+                    leftBound.X -= 1;
+                }
+
+                while (rightBound.X < pictureBox1.Width - 1 && equalColors(bitmap.GetPixel(leftBound.X, y), area_color))
+                {
+                    rightBound.X += 1;
+                }
+
+                drawLineBresenham(fill, leftBound.X, leftBound.Y, rightBound.X, rightBound.Y);
+
+                for (int i = leftBound.X; i < rightBound.X + 1; ++i)
+                {
+                    rec_fill(i, y + 1);
+                }
+                for (int i = leftBound.X; i < rightBound.X + 1; ++i)
+                {
+                    if (y > 0)
+                    {
+                        rec_fill(i, y - 1);
+                    }
+                }
+
+            }
+        }
+
+
+        //------------------------------- #2 -----------------------------------------------
+        bool bresenham = false;
+        bool wu = false;
+        bool line = false;
+        int line_st_x;
+        int line_st_y;
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            bresenham = true;
+            wu = false;
+            fill_flag = false;
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            bresenham = false;
+            wu = true;
+            fill_flag = false;
+        }
+        void drawLineBresenham(Pen pen, int x1, int y1, int x2, int y2)
+        {
+            Bitmap image = (Bitmap)pictureBox1.Image;
+            int deltaX = Math.Abs(x2 - x1);
+            int deltaY = Math.Abs(y2 - y1);
+            int signX = x1 < x2 ? 1 : -1;
+            int signY = y1 < y2 ? 1 : -1;
+            int error = deltaX - deltaY;
+
+            image.SetPixel(x2, y2, pen.Color);
+            pictureBox1.Invalidate();
+            while (x1 != x2 || y1 != y2)
+            {
+                image.SetPixel(x1, y1, pen.Color);
+                pictureBox1.Invalidate();
+                int error2 = error * 2;
+                if (error2 > -deltaY)
+                {
+                    error -= deltaY;
+                    x1 += signX;
+                }
+                if (error2 < deltaX)
+                {
+                    error += deltaX;
+                    y1 += signY;
+                }
+            }
+
+        }
+        void drawLineWu(Pen pen, int x1, int y1, int x2, int y2)
+        {
+            var image = (pictureBox1.Image as Bitmap);
+            double dx = x2 - x1; 
+            double dy = y2 - y1;
+            if (Math.Abs(dx) > Math.Abs(dy))
+            {
+                var gradient = dy / dx;
+                if (x2 < x1)
+                {
+                    var c = x1;
+                    x1 = x2;
+                    x2 = c;
+
+                    c = y1;
+                    y1 = y2;
+                    y2 = c;
+                }
+                var intery = y1 + gradient;
+                var xpxl1 = x1;
+                var xpxl2 = x2;
+                for (var x = xpxl1 + 1; x <= xpxl2 - 1; x++)
+                {
+                    image.SetPixel(x, (int)intery, Color.FromArgb((int)((1 - (intery - (int)intery)) * 255), pen.Color.R, pen.Color.G, pen.Color.B));
+                    image.SetPixel(x, (int)intery + 1, Color.FromArgb((int)((intery - (int)intery) * 255), pen.Color.R, pen.Color.G, pen.Color.B));
+                    pictureBox1.Invalidate();
+                    intery = intery + gradient;
+                }
+            }
+            else
+            {
+                if (y2 < y1)
+                {
+                    var c = x1;
+                    x1 = x2;
+                    x2 = c;
+
+                    c = y1;
+                    y1 = y2;
+                    y2 = c;
+                }
+
+                var gradient = dx / dy;
+                var interx = x1 + gradient;
+                var ypxl1 = y1;
+                var ypxl2 = y2;
+                for (var y = ypxl1 + 1; y <= ypxl2 - 1; y++)
+                {
+                    image.SetPixel((int)interx, y, Color.FromArgb((int)((1 - (interx - (int)interx)) * 255), pen.Color.R, pen.Color.G, pen.Color.B));
+                    image.SetPixel((int)interx + 1, y, Color.FromArgb((int)((interx - (int)interx) * 255), pen.Color.R, pen.Color.G, pen.Color.B));
+                    pictureBox1.Invalidate();
+                    interx = interx + gradient;
+                }
+            }
+        }
+
+
+        //------------------------------- clear ----------------------------------------------
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            graphics.Clear(Color.White);
+            pictureBox1.Invalidate();
+            fill_flag = false;
+        }
+
+        //------------------------------- color ----------------------------------------------
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+            {
+                pen.Color = colorDialog1.Color;
+                fill.Color = colorDialog1.Color;
+                pictureBox2.BackColor = colorDialog1.Color;
+            }
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            button1_Click(sender, e);
+        }
+
+        //---------------------------- pen --------------------------------------------------
+
+        int start_x;
+        int start_y;
+        bool drawing = false;
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            start_x = e.X;
+            start_y = e.Y;
+            drawing = true;
+
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            drawing = false;
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (drawing)
+            {
+                graphics.DrawLine(pen, start_x, start_y, e.X, e.Y);
+                start_x = e.X;
+                start_y = e.Y;
+                pictureBox1.Invalidate();
+            }
+        }
+
+        //-----------------------------------------------------------------------------------
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
